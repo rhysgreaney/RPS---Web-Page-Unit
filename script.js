4a1b5e56edaa1ce9d01e772fc7051023cf06ec57
rhysgreaney <126647880+rhysgreaney@users.noreply.github.com>
2023-03-13 17:11:12 +0800

    moved script to .js file

---
+++ script.js
@@ -0,0 +1,27 @@
+ /* I found this coding through https://dev.to/asaoluelijah/text-to-speech-in-3-lines-of-javascript-b8h */
+
+ function say(myMessage)
+ var msg = new SpeechSynthesisUtterance(myMessage);
+ window.speechSynthesis.speak(msg);
+
+
+ /* This is a simplified version of the game.
+The Computer ALWAYS picks ROCK.  
+Does the user's choose win,lose, or tie vs ROCK? */
+function play(user) {
+   let result = "";
+   if (user === 'rock') {
+       result = "tie";
+   }
+   if (user === 'paper') {
+       result = "win";
+   }
+   if (user === 'scissors') {
+       result = "lose";
+
+   var theMessage = "Computer chose rock, you "+ result +" + "!"
+   alert(theMessage);
+   say(theMessage);
+     }
+   alert("Computer chose rock, you " + result + "!");
+}
