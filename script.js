aea0d8466925d737537aeba3a9ac323371c1ca86
rhysgreaney <126647880+rhysgreaney@users.noreply.github.com>
2023-04-09 11:29:08 +0800

    indents

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
  /* This is a simplified version of the game.
 The Computer ALWAYS picks ROCK.  
 Does the user's choose win,lose, or tie vs ROCK? */
+function play(user) {
     let result = "";
     if (user === 'rock') {
         result = "tie";
